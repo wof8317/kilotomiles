c38f631d1c21ab722a08224645b2841a49d2e3c0
wof8317 <wof8317@gmail.com>
2022-08-31 15:17:00 -0500

    minor edits

--- kilotomiles/kilotomiles.cpp
+++ kilotomiles/kilotomiles.cpp
@@ -13,7 +13,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.If not, see < https://www.gnu.org/licenses/>.
+    along with this program.If not, see < https://www.gnu.org/licenses/ >.
+    You can contact the developer at wof8317@gmail.com.
 */
 #include <iostream>
 using namespace std;
